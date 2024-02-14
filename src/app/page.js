279b2a01a91ffb380af6f2a6a54b3136794bab38
Mehar Ali <mehar0alii@gmail.com>
2024-02-14 17:39:40 +0500

    background change

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import VideoPlayer from "@/components/VideoPlayer";
 
 export default function Home() {
   return (
-    <main className="flex w-screen h-screen flex-col items-center justify-center">
+    <main className="flex w-screen h-screen flex-col items-center justify-center bg-black">
       <VideoPlayer />
     </main>
   );
